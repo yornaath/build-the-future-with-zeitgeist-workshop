f966a805073d4ddf1c6a3692260c421ce617c378
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-05-23 17:08:57 +0200

    work

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,13 @@ import { $loaded, $sdk, $selectedAccount } from './state/wallet'
 const App = () => {
   const loaded = useStore($loaded)
 
-  return (
-    <Box maxW={'container.sm'}>
-      {!loaded ? (
-        <Box display={'flex'} m={88} justifyContent="center">
-          <Spinner />
-        </Box>
-      ) : (
-        <Box position={'fixed'} bottom="4" left="4" w="md">
-          <AccountSelector />
-        </Box>
-      )}
+  return !loaded ? (
+    <Box position={'fixed'} display={'flex'} h="100%" w="100%" alignItems="center" justifyContent="center">
+      <Spinner />
+    </Box>
+  ) : (
+    <Box position={'fixed'} bottom="4" left="4" w="md">
+      <AccountSelector />
     </Box>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import * as ReactDOM from 'react-dom/client'
 import App from './App'
 
 const rootElement = document.getElementById('root')
+
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>

6b274df11f8e8e352175cf11815da88d0ede4220
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-05-25 22:55:59 +0200

    Update gamestate.test.ts

--- packages/gamelogic/test/gamestate.test.ts
+++ packages/gamelogic/test/gamestate.test.ts
@@ -60,6 +60,19 @@ describe("GameState", () => {
       ],
       [
         [
+          [null, "x", null],
+          [null, "o", null],
+          ["o", "x", null],
+        ],
+        { player: challenged, coord: [2, 0] },
+        [
+          [null, "x", "o"],
+          [null, "o", null],
+          ["o", "x", null],
+        ],
+      ],
+      [
+        [
           ["o", null, "x"],
           [null, "x", "o"],
           [null, "o", null],
@@ -121,6 +134,32 @@ describe("GameState", () => {
           ["o", "x", null],
           ["o", "x", null],
           ["o", null, null],
+        ],
+      ],
+      [
+        [
+          [null, "o", "x"],
+          [null, "o", "x"],
+          [null, null, null],
+        ],
+        { player: challenged, coord: [1, 2] },
+        [
+          [null, "o", "x"],
+          [null, "o", "x"],
+          [null, "o", null],
+        ],
+      ],
+      [
+        [
+          [null, "x", "o"],
+          [null, "x", "o"],
+          [null, null, null],
+        ],
+        { player: challenged, coord: [2, 2] },
+        [
+          [null, "x", "o"],
+          [null, "x", "o"],
+          [null, null, "o"],
         ],
       ],
     ];

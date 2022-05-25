71b3f74b678f7253545bd4ffad6b343674665d3a
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-05-25 20:32:48 +0200

    game logic

--- packages/gamelogic/src/gameboard.ts
+++ packages/gamelogic/src/gameboard.ts
@@ -4,4 +4,12 @@ export type GameBoard = [
   [Slot, Slot, Slot]
 ];
 
-export type Slot = "x" | "o" | null;
+export type SlotRange = 0 | 1 | 2;
+
+export type Coordinate = [SlotRange, SlotRange];
+
+export type Challenger = "x";
+export type Challenged = "o";
+export type Empty = null;
+
+export type Slot = Challenger | Challenged | Empty;
--- packages/gamelogic/src/gamestate.ts
+++ packages/gamelogic/src/gamestate.ts
@@ -1,22 +1,102 @@
-import { GameBoard } from "./gameboard";
+import update from "immutability-helper";
+import { Coordinate, GameBoard, Slot } from "./gameboard";
 
-export type GameState = {};
+export type GameState = FreshGame | ProgressingGame | FinishedGame;
+
+export type FreshGame = GameStateBase & {
+  type: "fresh";
+};
+
+export type ProgressingGame = GameStateBase & {
+  type: "progressing";
+};
+
+export type FinishedGame = GameStateBase & {
+  type: "finished";
+  winner: string;
+};
 
 export type GameStateBase = {
-  type: GameStateType;
   players: {
     challenger: string;
     challenged: string;
   };
-};
-
-export type FreshGame = GameStateBase & {
-  type: "fresh";
   state: GameBoard;
+  events: string[];
 };
 
-export enum GameStateType {
-  fresh = "fresh",
-  inProgress = "progress",
-  finished = "finished",
-}
+export type Turn = {
+  player: string;
+  coord: Coordinate;
+};
+
+export const turn = (state: GameState, turn: Turn): GameState => {
+  if (!Object.keys(state.players).includes(turn.player)) {
+    return {
+      ...state,
+      events: [
+        ...state.events,
+        `${turn.player} tried to make a move, but isnt in the game. Ignored.`,
+      ],
+    };
+  }
+
+  if (state.type === "finished") {
+    return {
+      ...state,
+      events: [
+        ...state.events,
+        `${turn.player} tried to make a move when the game was finished`,
+      ],
+    };
+  }
+
+  if (state.type === "fresh" && turn.player === state.players.challenger) {
+    return {
+      ...state,
+      type: "finished",
+      winner: state.players.challenged,
+      events: [
+        ...state.events,
+        `Challenger ${state.players.challenger} tried to make the first move, resulting is loss.`,
+      ],
+    };
+  }
+
+  if (state.type === "fresh") {
+    return applyTurn(state, turn);
+  }
+
+  if (state.type === "progressing" && isPlayersTurn(state, turn)) {
+    return applyTurn(state, turn);
+  }
+
+  const winnerByCheating = Object.values(state.players).find(
+    (playerAddress) => playerAddress !== turn.player
+  ) as string;
+
+  return {
+    ...state,
+    type: "finished",
+    winner: winnerByCheating,
+    events: [
+      ...state.events,
+      `Player ${turn.player} tried to make a move out of turn, forfeiting the game.`,
+    ],
+  };
+};
+
+export const applyTurn = (state: GameState, turn: Turn): GameState => {
+  const [x, y] = turn.coord;
+  const slot: Slot = state.players.challenger === turn.player ? "x" : "o";
+  return update(state, { [y]: { $splice: [x, 1, slot] } });
+};
+
+export const isPlayersTurn = (state: GameState, turn: Turn): Boolean => {
+  const slots = state.state.flat();
+  const movesByChallenger = slots.filter((slot) => slot === "x");
+  const movesByChallenged = slots.filter((slot) => slot === "o");
+  const turnBelongsTo: keyof GameState["players"] =
+    movesByChallenged > movesByChallenger ? "challenger" : "challenged";
+  return turn.player === state.players[turnBelongsTo];
+};

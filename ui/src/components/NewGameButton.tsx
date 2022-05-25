0979407f01d285028df7e3990544147af487ee3a
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-05-25 15:09:28 +0200

    work work

--- ui/src/components/NewGameButton.tsx
+++ ui/src/components/NewGameButton.tsx
@@ -1,3 +1,4 @@
+import { CategoryMetadata, DecodedMarketMetadata } from '@zeitgeistpm/sdk/dist/types'
 import {
   Button,
   Modal,
@@ -9,16 +10,62 @@ import {
   ModalOverlay,
   useDisclosure,
 } from '@chakra-ui/react'
+import {
+  Box,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Text,
+} from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+import { isValid, lastFour, shortenAddress } from '../lib/account'
 import { useStore } from '@nanostores/react'
 import { IoLogoGameControllerA } from 'react-icons/io'
 import * as wallet from '../state/wallet'
-import { GameForm, NewGameForm } from './NewGameForm'
+
+export type GameForm = {
+  ammount: number
+  opponent: string
+}
 
 export const NewGameButton = () => {
   const sdk = useStore(wallet.$sdk)
+  const selectedAccount = useStore(wallet.$selectedAccount)
   const { isOpen, onOpen, onClose } = useDisclosure()
 
-  const onNewGameSubmitted = (game: GameForm) => {}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<GameForm>({
+    defaultValues: {
+      ammount: 10,
+      opponent: '',
+    },
+  })
+
+  const onNewGameSubmitted = (game: GameForm) => {
+    const metadata: DecodedMarketMetadata = {
+      question: 'Who will winn?',
+      description: 'FIGHT!',
+      slug: `tick-tack-block-${shortenAddress(selectedAccount)}-${shortenAddress(
+        game.opponent,
+      )}`,
+      categories: [
+        {
+          name: selectedAccount,
+          ticker: lastFour(selectedAccount),
+        },
+        {
+          name: game.opponent,
+          ticker: lastFour(game.opponent),
+        },
+      ],
+    }
+
+    sdk.models.createCpmmMarketAndDeployAssets
+  }
 
   return (
     <>
@@ -33,21 +80,51 @@ export const NewGameButton = () => {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent maxW="2xl">
-          <ModalHeader>Create a new game!</ModalHeader>
-          <ModalCloseButton />
+          <form onSubmit={handleSubmit(game => onNewGameSubmitted(game))}>
+            <ModalHeader>Create a new game!</ModalHeader>
+            <ModalCloseButton />
 
-          <ModalBody>
-            <NewGameForm onSubmit={onNewGameSubmitted} />
-          </ModalBody>
+            <ModalBody>
+              <Box>
+                <Box mb="4">
+                  <FormLabel display={'flex'}>
+                    <b>Ammount</b> <Text color="gray.500">(pool liquidity)</Text>
+                  </FormLabel>
+                  <InputGroup>
+                    <Input {...register('ammount')} type="number" />
+                    <InputRightAddon children={<b>ZBS</b>} />
+                  </InputGroup>
+                </Box>
 
-          <ModalFooter>
-            <Button colorScheme="green" mr={3}>
-              Create!
-            </Button>
-            <Button variant="ghost" onClick={onClose}>
-              Cancel
-            </Button>
-          </ModalFooter>
+                <Box mb="4">
+                  <FormLabel display={'flex'}>
+                    <b>Opponent</b> <Text color="gray.500">(address)</Text>
+                  </FormLabel>
+                  <Input
+                    {...register('opponent', {
+                      required: true,
+                      validate: { valid: isValid },
+                    })}
+                    type="string"
+                  />
+                  {errors.opponent && (
+                    <Text mt="1" color="red">
+                      Invalid address
+                    </Text>
+                  )}
+                </Box>
+              </Box>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button type="submit" colorScheme="green" mr={3}>
+                Create!
+              </Button>
+              <Button variant="ghost" onClick={onClose}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </form>
         </ModalContent>
       </Modal>
     </>

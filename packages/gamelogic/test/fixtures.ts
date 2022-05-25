91a862cd72abf1fad4cddce02933f37262e64fdf
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-05-25 22:57:38 +0200

    move moves to fixtures

---
+++ packages/gamelogic/test/fixtures.ts
@@ -0,0 +1,124 @@
+import { GameBoard, Turn } from "../src";
+
+export const challenger = "foo";
+export const challenged = "bar";
+
+export const moves: [GameBoard, Turn, GameBoard][] = [
+  [
+    [
+      ["o", null, "x"],
+      [null, "o", null],
+      [null, "x", null],
+    ],
+    { player: challenged, coord: [2, 2] },
+    [
+      ["o", null, "x"],
+      [null, "o", null],
+      [null, "x", "o"],
+    ],
+  ],
+  [
+    [
+      [null, "x", null],
+      [null, "o", null],
+      ["o", "x", null],
+    ],
+    { player: challenged, coord: [2, 0] },
+    [
+      [null, "x", "o"],
+      [null, "o", null],
+      ["o", "x", null],
+    ],
+  ],
+  [
+    [
+      ["o", null, "x"],
+      [null, "x", "o"],
+      [null, "o", null],
+    ],
+    { player: challenger, coord: [0, 2] },
+    [
+      ["o", null, "x"],
+      [null, "x", "o"],
+      ["x", "o", null],
+    ],
+  ],
+  [
+    [
+      ["o", "o", null],
+      [null, null, null],
+      [null, "x", "x"],
+    ],
+    { player: challenged, coord: [2, 0] },
+    [
+      ["o", "o", "o"],
+      [null, null, null],
+      [null, "x", "x"],
+    ],
+  ],
+  [
+    [
+      [null, null, null],
+      ["o", "o", null],
+      [null, "x", "x"],
+    ],
+    { player: challenged, coord: [2, 1] },
+    [
+      [null, null, null],
+      ["o", "o", "o"],
+      [null, "x", "x"],
+    ],
+  ],
+  [
+    [
+      [null, null, null],
+      [null, "x", "x"],
+      ["o", "o", null],
+    ],
+    { player: challenged, coord: [2, 2] },
+    [
+      [null, null, null],
+      [null, "x", "x"],
+      ["o", "o", "o"],
+    ],
+  ],
+  [
+    [
+      ["o", "x", null],
+      ["o", "x", null],
+      [null, null, null],
+    ],
+    { player: challenged, coord: [0, 2] },
+    [
+      ["o", "x", null],
+      ["o", "x", null],
+      ["o", null, null],
+    ],
+  ],
+  [
+    [
+      [null, "o", "x"],
+      [null, "o", "x"],
+      [null, null, null],
+    ],
+    { player: challenged, coord: [1, 2] },
+    [
+      [null, "o", "x"],
+      [null, "o", "x"],
+      [null, "o", null],
+    ],
+  ],
+  [
+    [
+      [null, "x", "o"],
+      [null, "x", "o"],
+      [null, null, null],
+    ],
+    { player: challenged, coord: [2, 2] },
+    [
+      [null, "x", "o"],
+      [null, "x", "o"],
+      [null, null, "o"],
+    ],
+  ],
+];
--- packages/gamelogic/test/gamestate.test.ts
+++ packages/gamelogic/test/gamestate.test.ts
@@ -1,8 +1,6 @@
 import * as GS from "../src/gamestate";
 import * as GB from "../src/gameboard";
-
-const challenger = "foo";
-const challenged = "bar";
+import { challenged, challenger, moves } from "./fixtures";
 
 const createFreshState = () => {
   const state: GS.GameState = {
@@ -44,126 +42,6 @@ describe("GameState", () => {
   });
 
   test("Winning moves", () => {
-    const moves: [GB.GameBoard, GS.Turn, GB.GameBoard][] = [
-      [
-        [
-          ["o", null, "x"],
-          [null, "o", null],
-          [null, "x", null],
-        ],
-        { player: challenged, coord: [2, 2] },
-        [
-          ["o", null, "x"],
-          [null, "o", null],
-          [null, "x", "o"],
-        ],
-      ],
-      [
-        [
-          [null, "x", null],
-          [null, "o", null],
-          ["o", "x", null],
-        ],
-        { player: challenged, coord: [2, 0] },
-        [
-          [null, "x", "o"],
-          [null, "o", null],
-          ["o", "x", null],
-        ],
-      ],
-      [
-        [
-          ["o", null, "x"],
-          [null, "x", "o"],
-          [null, "o", null],
-        ],
-        { player: challenger, coord: [0, 2] },
-        [
-          ["o", null, "x"],
-          [null, "x", "o"],
-          ["x", "o", null],
-        ],
-      ],
-      [
-        [
-          ["o", "o", null],
-          [null, null, null],
-          [null, "x", "x"],
-        ],
-        { player: challenged, coord: [2, 0] },
-        [
-          ["o", "o", "o"],
-          [null, null, null],
-          [null, "x", "x"],
-        ],
-      ],
-      [
-        [
-          [null, null, null],
-          ["o", "o", null],
-          [null, "x", "x"],
-        ],
-        { player: challenged, coord: [2, 1] },
-        [
-          [null, null, null],
-          ["o", "o", "o"],
-          [null, "x", "x"],
-        ],
-      ],
-      [
-        [
-          [null, null, null],
-          [null, "x", "x"],
-          ["o", "o", null],
-        ],
-        { player: challenged, coord: [2, 2] },
-        [
-          [null, null, null],
-          [null, "x", "x"],
-          ["o", "o", "o"],
-        ],
-      ],
-      [
-        [
-          ["o", "x", null],
-          ["o", "x", null],
-          [null, null, null],
-        ],
-        { player: challenged, coord: [0, 2] },
-        [
-          ["o", "x", null],
-          ["o", "x", null],
-          ["o", null, null],
-        ],
-      ],
-      [
-        [
-          [null, "o", "x"],
-          [null, "o", "x"],
-          [null, null, null],
-        ],
-        { player: challenged, coord: [1, 2] },
-        [
-          [null, "o", "x"],
-          [null, "o", "x"],
-          [null, "o", null],
-        ],
-      ],
-      [
-        [
-          [null, "x", "o"],
-          [null, "x", "o"],
-          [null, null, null],
-        ],
-        { player: challenged, coord: [2, 2] },
-        [
-          [null, "x", "o"],
-          [null, "x", "o"],
-          [null, null, "o"],
-        ],
-      ],
-    ];
-
     for (const [start, turn, end] of moves) {
       const state: GS.GameState = {
         type: "progressing",
